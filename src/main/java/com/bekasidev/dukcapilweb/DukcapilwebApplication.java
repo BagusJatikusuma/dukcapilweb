e8ebd09bf8487782c9337903bac3955084b1e4ff
Bagus Jatikusuma <jatikusuma0801@gmail.com>
2018-10-12 08:54:49 +0700

    commit 12 okt 2018 - bagus

--- src/main/java/com/bekasidev/dukcapilweb/DukcapilwebApplication.java
+++ src/main/java/com/bekasidev/dukcapilweb/DukcapilwebApplication.java
@@ -3,6 +3,11 @@ package com.bekasidev.dukcapilweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ *
+ * main class for spring boot
+ *
+ */
 @SpringBootApplication
 public class DukcapilwebApplication {
 
