7dd7335fb34666ccb0b9da05475ca9ba111d380d
Bagus Jatikusuma <jatikusuma0801@gmail.com>
2018-10-12 08:50:15 +0700

    First commit

---
+++ src/main/java/com/bekasidev/dukcapilweb/DukcapilwebApplication.java
@@ -0,0 +1,12 @@
+package com.bekasidev.dukcapilweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DukcapilwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DukcapilwebApplication.class, args);
+	}
+}
